20d4534698f9b701797d83259e285b560111dba1
Lavanya Sundarapu <lavanyadevika1122@gmail.com>
2026-03-13 18:35:18 +0530

    first commit

---
+++ src/main/java/com/example/demo/Csecontroller.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Csecontroller {
+	@GetMapping("/add")
+    public int cseadd(@RequestParam int a, @RequestParam int b) 
+	{
+        return a + b;
+    }
+
+    @GetMapping("/mul")
+    public int csemul(@RequestParam int a, @RequestParam int b) 
+    {
+        return a * b;
+    }
+}
